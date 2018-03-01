db04db9e7ba7e69aeee9b82bd030b5e4b8990f58
Kanstantsin Kamkou <me@2ka.by>
2018-03-01 12:12:49 +0100

    code cleanup

--- src/components/widgets/ThingCharts.vue
+++ src/components/widgets/ThingCharts.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="tile is-parent">
-
     <span v-if="loading" class="icon is-large"><i class="ion-clock"></i></span>
-    <article class="tile is-child box">
+
+    <article v-show="!loading" class="tile is-child box">
       <p class="title">Activity</p>
-      <figure class=" is-4by3">
-        <div id="chart_div"></div>
-      </figure>
+      <figure ref="chart"></figure>
     </article>
   </div>
 </template>
@@ -18,50 +16,56 @@
     name: 'widget-thing-charts',
     props: {entity: Object},
     data() {
-      return {loading: true};
+      return {loading: true, payload: []};
+    },
+    beforeMount() {
+      google.charts.load('current', {packages: ['corechart', 'bar']});
     },
     async mounted() {
-      google.charts.load('current', {packages: ['corechart', 'bar']});
-
-
-      function drawStacked(payload) {
-        var data = new google.visualization.DataTable();
-        data.addColumn('number', 'Day Of Month');
-        data.addColumn('number', 'Driving');
-        data.addColumn('number', 'Standstill');
-
-        payload.forEach(e =>
-          data.addRow([
-            e.dayOfMonth,
-            moment.duration(e.gpsDriveDurationS, 's').asMinutes(),
-            moment.duration(e.gpsStayDurationS, 's').asMinutes()
-          ])
-        );
-
-
-        var options = {
-          bars: 'horizontal',
-          chartArea: {width: '80%', height: '80%'},
-          legend: {position: 'none'},
-          isStacked: true
-        };
-
-      var chart = new google.visualization.BarChart(document.getElementById('chart_div'));
-      chart.draw(data, options);
-      }
-
       try {
         const response = await this.$store.getters['common/apiInsiderProgram']
           .statisticsStandstill(this.entity._id);
-
-        google.charts.setOnLoadCallback(() => drawStacked(response.data));
+        this.payload = response.data;
       } catch (e) {
         console.error(e);
       } finally {
         this.loading = false;
       }
     },
+    watch: {
+      loading (current, previous) {
+        if (current) { return; }
+        this.chartRepaint();
+      }
+    },
     methods: {
+      chartRepaint() {
+        const data = new google.visualization.DataTable();
+        data.addColumn('date', 'Date');
+        data.addColumn('number', 'Driving');
+        data.addColumn({type: 'string', role: 'tooltip'});
+        data.addColumn('number', 'Standstill');
+        data.addColumn({type: 'string', role: 'tooltip'});
+
+        this.payload.forEach(e => {
+          const driveDuration = moment.duration(e.geoDriveDurationS, 's'),
+            stayDuration = moment.duration(e.geoStayDurationS, 's');
+          data.addRow([
+            moment().dayOfYear(e.dayOfYear).toDate(),
+            driveDuration.asMinutes(),
+            `${moment.utc(driveDuration.asMilliseconds()).format("HH:mm")}h`,
+            stayDuration.asMinutes(),
+            `${moment.utc(stayDuration.asMilliseconds()).format("HH:mm")}h`,
+          ]);
+        });
+
+        (new google.visualization.BarChart(this.$refs.chart)).draw(data, {
+          chartArea: {width: '60%', height: '90%'},
+          legend: {position: 'none'},
+          hAxis: {textPosition: 'none', baselineColor: '#039be5', gridlines: {color: 'none'}},
+          isStacked: true
+        });
+      }
     }
   }
 </script>
