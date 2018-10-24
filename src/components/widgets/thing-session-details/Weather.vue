1d12f4c62c318bc0b78cc12891757e3749c315fc
Kanstantsin Kamkou <me@2ka.by>
2018-10-24 13:21:00 +0200

    added weather direction

--- src/components/widgets/thing-session-details/Weather.vue
+++ src/components/widgets/thing-session-details/Weather.vue
@@ -3,18 +3,21 @@
     <span class="icon is-large"><i class="ion-ios-time"></i></span>
   </div>
 
-  <article v-else-if="weather.length" class="content">
-    <template v-if="weather[0].payload.visibility">
+  <article v-else-if="weather" class="content">
+    <template v-if="weather.visibility">
       Visibility
-      <span class="tag">{{ Math.round(weather[0].payload.visibility / 1000) }} km</span>.
+      <span class="tag">{{ Math.round(weather.visibility / 1000) }} km</span>.
     </template>
     Weather might be described as
-    <span class="tag">{{ weather[0].payload.weather[0].description }}</span> with
-    <span class="tag">{{ weather[0].payload.wind.speed }} m/s wind</span>.
+    <span class="tag">{{ weather.weather[0].description }}</span> with
+    <span class="tag">{{ weather.wind.speed }} m/s wind</span>
+    <span :style="[{display: 'inline-block'}, {transform: `rotate(${weather.wind.deg}deg)`}]">
+      <i class="ion-ios-arrow-round-up has-text-info"></i>
+    </span>.
     The temperature was
-    <span class="tag">{{ Math.round(weather[0].payload.main.temp) }}&#8451;</span>,
-    humidity <span class="tag">{{ weather[0].payload.main.humidity }}%</span> and
-    pressure <span class="tag">{{ weather[0].payload.main.pressure }} hPa</span>.
+    <span class="tag">{{ Math.round(weather.main.temp) }}&#8451;</span>,
+    humidity <span class="tag">{{ weather.main.humidity }}%</span> and
+    pressure <span class="tag">{{ weather.main.pressure }} hPa</span>.
   </article>
 
   <div v-else class="notification">No weather information available.</div>
@@ -24,9 +27,7 @@
   export default {
     name: 'thing-session-details-weather',
     props: {sessionId: String},
-    data() {
-      return {loading: true, weather: []};
-    },
+    data: () => ({loading: true, entries: []}),
     mounted() {
       this.fetchData(this.sessionId);
     },
@@ -39,13 +40,19 @@
       async fetchData(id) {
         this.loading = true;
         try {
-          this.weather = (await this.$store.getters['common/apiInsiderProgram']
+          this.entries = (await this.$store.getters['common/apiInsiderProgram']
             .sessionWeather(id)).data;
         } catch (e) {
           console.error(e);
+          return;
         } finally {
           this.loading = false;
         }
+      }
+    },
+    computed: {
+      weather() {
+        return this.entries.length ? this.entries[0].payload : null;
       }
     }
   }
