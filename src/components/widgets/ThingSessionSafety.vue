cac02469cb9ede64da102b6103fd2306c1acb837
Kanstantsin Kamkou <me@2ka.by>
2018-11-11 17:41:01 +0100

    safety chart polishing

--- src/components/widgets/ThingSessionSafety.vue
+++ src/components/widgets/ThingSessionSafety.vue
@@ -1,12 +1,17 @@
 <template>
   <article class="tile is-child is-radiusless box" style="position: relative;">
+    <h6 class="subtitle">Trip safety</h6>
     <div v-if="loading" class="has-text-centered">
       <span class="icon is-large"><i class="ion-ios-time"></i></span>
     </div>
 
-    <div v-else class="columns is-gapless">
-      <div ref="chart"></div>
+    <div v-else-if="!isPayloadEmpty" class="columns is-gapless">
+      <div class="column">
+        <div ref="chart"></div>
+      </div>
     </div>
+
+    <p v-else class="notification has-text-centered">Not enough data to build the safety chart.</p>
 
     <feedback style="position: absolute; bottom: 0; left: 0;"
       :widget-version="widgetVersion"
@@ -72,15 +77,20 @@
 
         const chart = new google.visualization.PieChart(this.$refs.chart),
           options = {
-            pieHole: 0.5,
-            legend: 'none',
+            pieHole: 0.4,
+            legend: {position: 'labeled', textStyle: {color: '#363636'}},
+            pieStartAngle: 100,
+            enableInteractivity: false,
+            pieSliceText: 'none',
+            title: 'none',
+            colors: ['#00b89c', '#f46036', '#ff3860'],
             slices: {
-              0: {color: '#00b89c'},
-              1: {color: '#f46036'},
-              2: {color: '#ff3860'}
+              0: {},
+              1: {offset: 0.1},
+              2: {offset: 0.3}
             },
-            width: 300,
-            chartArea:{left: '30%', top: '5%', width: '100%', height: '90%'}
+            height: 160,
+            chartArea:{left: '10', top: '8', width: '100%', height: '90%'}
           };
 
         chart.draw(dataTable, options);
@@ -89,6 +99,9 @@
     computed: {
       widgetDebugData() {
         return _(this.$data).omit(['api']).merge(this.$props).value();
+      },
+      isPayloadEmpty() {
+        return !this.payload || !_(this.payload).values().sumBy('count');
       }
     }
   }
